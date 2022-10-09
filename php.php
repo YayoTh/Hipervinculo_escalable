9c09b52a8ba61e3b94ebd2f2d81cd9f949674fc8
Yayo <113472320+YayoTh@users.noreply.github.com>
2022-10-09 12:25:22 +0200

    Hipervinculo escalable

---
+++ php.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Inicio de PhP</title>
+        <style>
+            #contenedor{
+                margin: 13px;
+                padding: 13px;
+            }
+        </style>
+    </head>
+    <body>
+        
+    </body>
+</html>
